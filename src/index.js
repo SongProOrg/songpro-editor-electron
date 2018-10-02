9a7b94e190a4f7126aabcfb037c34b416a0beba4
Brian Kelly <polymonic@gmail.com>
2018-10-02 08:32:46 -0400

    Remove unused App class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import SongProEditor from 'songpro-editor-component';
 
 ReactDOM.render(<SongProEditor />, document.getElementById('root'));
